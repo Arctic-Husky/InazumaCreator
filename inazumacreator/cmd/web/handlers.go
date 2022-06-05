3d0dddc413c4e689276c53718ee1a7ae9cabd76f
ArcticHusky <>
2022-06-05 22:01:28 +0000

    Feito createPersonagem

--- inazumacreator/cmd/web/handlers.go
+++ inazumacreator/cmd/web/handlers.go
@@ -3,11 +3,12 @@ package main
 //go run cmd/web/*
 import (
 	"fmt"
-	//"html/template"
+	"html/template"
 	"net/http"
 	"strconv"
+
   
-  "github.com/Arctic-Husky/Aprendendo-GO/pkg/models"
+  "github.com/Arctic-Husky/InazumaCreator/pkg/models"
 )
 
 func(app *application) home(rw http.ResponseWriter, r *http.Request){
@@ -16,36 +17,44 @@ func(app *application) home(rw http.ResponseWriter, r *http.Request){
     return
   } 
 
-  // files := []string{
-  //   "./ui/html/home.page.tmpl.html",
-  //   "./ui/html/base.layout.tmpl.html",
-  //   "./ui/html/footer.partial.tmpl.html",
-  // }
-  // ts, err := template.ParseFiles(files...)
-  // if err != nil{
-  //   app.serverError(rw, err)
-  //   // app.errorLog.Println(err.Error())
-  //   // http.Error(rw, "Internal Error",500)
-  //   return
-  // }
-  // err = ts.Execute(rw, nil)
-  // if err != nil{
-  //   app.serverError(rw, err)
-  //   return
-  // }
-
-  snippets, err := app.snippets.Latest()
+  personagens, err := app.personagens.Latest()
   if err != nil{
     app.serverError(rw,err)
     return
   }
-  for _,s := range snippets{
-    fmt.Fprintf(rw, "%v \n",s)
+
+  // MUDAR AQUI
+  files := []string{
+    "./ui/html/home.tmpl.html",
+    "./ui/html/footer.tmpl.html",
+    "./ui/html/top.tmpl.html",
+    "./ui/html/base.tmpl.html",
   }
+  ts, err := template.ParseFiles(files...)
+  if err != nil{
+    app.serverError(rw, err)
+    // app.errorLog.Println(err.Error())
+    // http.Error(rw, "Internal Error",500)
+    return
+  }
+  err = ts.Execute(rw, personagens)
+  if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+
+  // personagens, err := app.personagens.Latest()
+  // if err != nil{
+  //   app.serverError(rw,err)
+  //   return
+  // }
+  // for _,s := range personagens{
+  //   fmt.Fprintf(rw, "%v \n",s)
+  // }
 }
 
 //http://localhost:4000/snippet?id=123
-func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
+func(app *application) showPersonagem(rw http.ResponseWriter, r *http.Request){
   id,err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
     app.notFound(rw)
@@ -53,7 +62,7 @@ func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
     return
   }
 
-  s, err := app.snippets.Get(id)
+  s, err := app.personagens.Get(id)
   if err == models.ErrNoRecord{
     app.notFound(rw)
     return
@@ -63,27 +72,99 @@ func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
   }
 
   fmt.Fprintf(rw, "%v",s)
+}
+
+func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
+  id,err := strconv.Atoi(r.URL.Query().Get("id"))
+  if err != nil || id < 1 {
+    app.notFound(rw)
+    // http.NotFound(rw, r)
+    return
+  }
+
+  s, err := app.personagens.Get(id)
+  if err == models.ErrNoRecord{
+    app.notFound(rw)
+    return
+  }else if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+
+  files := []string{
+    "./ui/html/show.tmpl.html",
+    "./ui/html/base.tmpl.html",
+    "./ui/html/footer.tmpl.html",
+    "./ui/html/top.tmpl.html",
+  }
+  ts, err := template.ParseFiles(files...)
+  if err != nil{
+    app.serverError(rw, err)
+    // app.errorLog.Println(err.Error())
+    // http.Error(rw, "Internal Error",500)
+    return
+  }
+  err = ts.Execute(rw, s)
+  if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+
+  fmt.Fprintf(rw, "%v",s)
   
   //fmt.Fprintf(rw, "Exibir o Snippet de ID: %d", id)
 }
 
-func(app *application) createSnippet(rw http.ResponseWriter, r *http.Request){
-  if r.Method != "POST"{
-    rw.Header().Set("Allow","POST")
-    app.clientError(rw, http.StatusMethodNotAllowed)
-    // http.Error(rw, "Metodo não permitido", http.StatusMethodNotAllowed)
-    return
+func (app *application) createPersonagem(rw http.ResponseWriter, r *http.Request){
+  files := []string{
+    "./ui/html/creator.tmpl.html",
+    "./ui/html/footer.tmpl.html",
+    "./ui/html/top.tmpl.html",
+    "./ui/html/base.tmpl.html",
   }
-
-  title := "ALMOÇO DE HJ"
-  content := "RICKS BUERGUE RICKS BURGIER"
-  expires := "7"
-
-  id, err := app.snippets.Insert(title,content,expires)
+  ts, err := template.ParseFiles(files...)
   if err != nil{
-    app.serverError(rw,err)
+    app.serverError(rw, err)
     return
   }
+  err = ts.Execute(rw, nil)
+  if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+  // nome := "Teste"
+  // hab1 := "god hand"
+  // hab2 := "back tornado"
+  // posicao := "FW"
+  // elemento := "Terra"
+  // expires := "7"
 
-  http.Redirect(rw,r,fmt.Sprintf("/snippet:id=%d",id), http.StatusSeeOther)
+  // id, err := app.personagens.Insert(nome,hab1,hab2,posicao,elemento,expires)
+  // if err != nil{
+  //   app.serverError(rw,err)
+  //   return
+  // }
+
+  // http.Redirect(rw,r,fmt.Sprintf("/personagem?id=%d",id), http.StatusSeeOther)
 }
+
+// func(app *application) createSnippet(rw http.ResponseWriter, r *http.Request){
+//   if r.Method != "POST"{
+//     rw.Header().Set("Allow","POST")
+//     app.clientError(rw, http.StatusMethodNotAllowed)
+//     // http.Error(rw, "Metodo não permitido", http.StatusMethodNotAllowed)
+//     return
+//   }
+
+//   title := "ALMOÇO DE HJ"
+//   content := "RICKS BUERGUE RICKS BURGIER"
+//   expires := "7"
+
+//   id, err := app.personagens.Insert(title,content,expires)
+//   if err != nil{
+//     app.serverError(rw,err)
+//     return
+//   }
+
+//   http.Redirect(rw,r,fmt.Sprintf("/snippet:id=%d",id), http.StatusSeeOther)
+// }
