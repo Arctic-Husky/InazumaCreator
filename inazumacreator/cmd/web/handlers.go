6335b3c907bdcec783b61638ca7fd7426722a85f
marcelinnn <62546620+Marcelinnn@users.noreply.github.com>
2022-06-06 19:33:05 +0000

    Adicionado página de bem vindo

--- inazumacreator/cmd/web/handlers.go
+++ inazumacreator/cmd/web/handlers.go
@@ -22,11 +22,32 @@ type personagem struct{
   Expires time.Time
 }
 
-func(app *application) home(rw http.ResponseWriter, r *http.Request){
+func(app *application) welcome(rw http.ResponseWriter, r *http.Request){
   if r.URL.Path != "/"{
     app.notFound(rw)
     return
   } 
+  
+  files := []string{
+    "./ui/html/welcome.tmpl.html",
+    "./ui/html/footer.tmpl.html",
+    "./ui/html/topPrincipal.tmpl.html",
+    "./ui/html/base.tmpl.html",
+  }
+  ts, err := template.ParseFiles(files...)
+  if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+  err = ts.Execute(rw, nil)
+  if err != nil{
+    app.serverError(rw, err)
+    return
+  }
+}
+
+func(app *application) home(rw http.ResponseWriter, r *http.Request){
+  
 
   personagens, err := app.personagens.Latest()
   if err != nil{
@@ -102,47 +123,6 @@ func(app *application) showPersonagem(rw http.ResponseWriter, r *http.Request){
   //fmt.Fprintf(rw, "%v",s)
 }
 
-func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
-  id,err := strconv.Atoi(r.URL.Query().Get("id"))
-  if err != nil || id < 1 {
-    app.notFound(rw)
-    // http.NotFound(rw, r)
-    return
-  }
-
-  s, err := app.personagens.Get(id)
-  if err == models.ErrNoRecord{
-    app.notFound(rw)
-    return
-  }else if err != nil{
-    app.serverError(rw, err)
-    return
-  }
-
-  files := []string{
-    "./ui/html/show.tmpl.html",
-    "./ui/html/base.tmpl.html",
-    "./ui/html/footer.tmpl.html",
-    "./ui/html/top.tmpl.html",
-  }
-  ts, err := template.ParseFiles(files...)
-  if err != nil{
-    app.serverError(rw, err)
-    // app.errorLog.Println(err.Error())
-    // http.Error(rw, "Internal Error",500)
-    return
-  }
-  err = ts.Execute(rw, s)
-  if err != nil{
-    app.serverError(rw, err)
-    return
-  }
-
-  fmt.Fprintf(rw, "%v",s)
-  
-  //fmt.Fprintf(rw, "Exibir o Snippet de ID: %d", id)
-}
-
 func (app *application) createPersonagem(rw http.ResponseWriter, r *http.Request){
 
   pers := personagem{
@@ -184,47 +164,4 @@ func (app *application) createPersonagem(rw http.ResponseWriter, r *http.Request
     app.serverError(rw, err)
     return
   }
-  
-  
-  // nome := "Teste"
-  // hab1 := "god hand"
-  // hab2 := "back tornado"
-  // posicao := "FW"
-  // elemento := "Terra"
-  // expires := "7"
-
-  // id, err := app.personagens.Insert(nome,hab1,hab2,posicao,elemento,expires)
-  // if err != nil{
-  //   app.serverError(rw,err)
-  //   return
-  // }
-
-  // http.Redirect(rw,r,fmt.Sprintf("/personagem?id=%d",id), http.StatusSeeOther)
 }
-
-// func(app *application) registrarPersonagem(rw http.ResponseWriter, r *http.Request){
-  
-//   r.ParseForm()
-//   fmt.Fprintf(rw, "%v", r.Form)
-// }
-
-// func(app *application) createSnippet(rw http.ResponseWriter, r *http.Request){
-//   if r.Method != "POST"{
-//     rw.Header().Set("Allow","POST")
-//     app.clientError(rw, http.StatusMethodNotAllowed)
-//     // http.Error(rw, "Metodo não permitido", http.StatusMethodNotAllowed)
-//     return
-//   }
-
-//   title := "ALMOÇO DE HJ"
-//   content := "RICKS BUERGUE RICKS BURGIER"
-//   expires := "7"
-
-//   id, err := app.personagens.Insert(title,content,expires)
-//   if err != nil{
-//     app.serverError(rw,err)
-//     return
-//   }
-
-//   http.Redirect(rw,r,fmt.Sprintf("/snippet:id=%d",id), http.StatusSeeOther)
-// }
--- inazumacreator/cmd/web/routes.go
+++ inazumacreator/cmd/web/routes.go
@@ -7,9 +7,10 @@ import(
 func (app *application) routes() *http.ServeMux {
   mux := http.NewServeMux()
   
-  mux.HandleFunc("/", app.home)
+  mux.HandleFunc("/", app.welcome)
   mux.HandleFunc("/personagem", app.showPersonagem)
   mux.HandleFunc("/personagem/create", app.createPersonagem)
+  mux.HandleFunc("/lista", app.home)
 
   fileServer := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/",http.StripPrefix("/static",fileServer))
